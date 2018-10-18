8593ad27bdeb0dbe0f4092ccd66cc5f0104fbb39
chatterboy <chatter0502@gmail.com>
2018-10-18 15:31:24 +0900

    [#17] Modify Load class
    - Do refactoring

--- utils/Load.py
+++ utils/Load.py
@@ -2,7 +2,7 @@ import os
 import cv2
 
 class Load:
-    def load_with_labels(self, path):
+    def load(self, path):
         """
         :param path: a string
         :return self.images: a list of numpy
@@ -12,13 +12,12 @@ class Load:
         self.labels = []
         # path내에 labels.txt가 있다는 것을 보장해야함
         with open(os.path.join(path, 'labels.txt'), 'r') as f:
-            for l in f.read().split('\n'):
-                p = l.split('\t')
-                img = cv2.imread(os.path.join(path, p[0]))
-                # 우선, CT 이미지만 로드
-                if img.shape[0] == 512 and img.shape[1] == 512:
-                    self.images.append(img)
-                    self.labels.append([int(i) for i in p[1].split(',')])
+            for line in f.readlines():
+                line = line[:-1]
+                fname, labels = line.split('\t')
+                img = cv2.imread(os.path.join(path, fname))
+                self.images.append(img)
+                self.labels.append([int(i) for i in labels.split(',')])
 
     def get(self):
         """
